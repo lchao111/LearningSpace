93a6c7f10ab2a3f47343385aec9d4ba4918bff7a
Chao Li <l.chaoneu@hotmail.com>
2018-05-09 14:29:30 -0700

    learning N rules.

---
+++ LearningNRules/Rules.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rules.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rules.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5be04eb3-349c-4e74-9b06-6d961d49e713")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LearningNRules/Rules/PreferredCustomerDiscountRule.cs
@@ -0,0 +1,38 @@
+﻿using LearningNRules;
+using NRules.Fluent.Dsl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rules
+{
+    public class PreferredCustomerDiscountRule : Rule
+    {
+        public override void Define()
+        {
+            Customer customer = null;
+            IEnumerable<Order> orders = null;
+
+            When()
+                .Match<Customer>(() => customer, c => c.IsPreferred)
+                .Query(() => orders, x => x
+                    .Match<Order>(
+                        o => o.Customer == customer,
+                        o => !o.IsDiscounted)
+                    .Collect()
+                    .Where(c => c.Any()));
+
+            Then()
+                .Do(ctx => ApplyDiscount(orders, 10.0))
+                .Do(ctx => ctx.UpdateAll(orders));
+        }
+
+        private static void ApplyDiscount(IEnumerable<Order> orders, double discount)
+        {
+            foreach (var order in orders)
+            {
+                order.ApplyDiscount(discount);
+            }
+        }
+    }
+}
