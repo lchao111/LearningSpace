3328e11239a7e7c7fd962ef2347c963ddbe04c04
Chao Li <l.chaoneu@hotmail.com>
2018-05-09 15:05:02 -0700

    try quick Nrules by follow example.

---
+++ LearningNRules/Rules.Tests/PreferredCustomerDiscountRuleTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using LearningNRules;
+using NRules.Fluent;
+using NRules;
+using Xunit;
+
+namespace Rules.Tests
+{
+    public class PreferredCustomerDiscountRuleTests
+    {
+        [Fact]
+        public void TestRules()
+        {
+            var repository = new RuleRepository();
+            repository.Load(x => x.From(typeof(PreferredCustomerDiscountRule).Assembly));
+
+            //Compile rules
+            var factory = repository.Compile();
+
+            //Create a working session
+            var session = factory.CreateSession();
+
+            //Load domain model
+            var customer = new Customer("John Doe") { IsPreferred = true };
+            var order1 = new Order(123456, customer, 2, 25.0);
+            var order2 = new Order(123457, customer, 1, 100.0);
+
+            //Insert facts into rules engine's memory
+            session.Insert(customer);
+            session.Insert(order1);
+            session.Insert(order2);
+
+            //Start match/resolve/act cycle
+            session.Fire();
+        }
+    }
+}
---
+++ LearningNRules/Rules/DicsountNotificationRule.cs
@@ -0,0 +1,23 @@
+﻿using LearningNRules;
+using NRules.Fluent.Dsl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rules
+{
+    public class DicsountNotificationRule : Rule
+    {
+        public override void Define()
+        {
+            Customer customer = null;
+
+            When()
+                .Match<Customer>(() => customer)
+                .Exists<Order>(o => o.Customer == customer, o => o.PercentDiscount > 0.0);
+
+            Then()
+                .Do(_ => customer.NotifyAboutDiscount());
+        }
+    }
+}
